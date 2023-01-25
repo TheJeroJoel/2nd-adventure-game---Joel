bc7af45c795fe303356a23175fe78fd12c06e18d
Joel Schlueter <joelschl787@ed.amdsb.ca>
2023-01-25 10:28:41 -0500

    Add project files.

---
+++ 2nd adventure game - Joel/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace _2nd_adventure_game___Joel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
